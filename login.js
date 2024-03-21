05554cd32aeb4be01ef61e376e64e8e3ccdbe2b6
DESKTOP-M3UJ6SC\abels <szolnokiabi25@gmail.com>
2024-03-21 20:39:19 +0100

    Login szempontok kész

--- login.js
+++ login.js
@@ -6,11 +6,13 @@ const emailInput = document.querySelector('#email')
 const error = document.querySelector('.errorMessege')
 const errorP = document.querySelector('.errorMessege p')
 
+const user_Email = ['gaspardani719@gmail.com', 'abelszolnoki@gmail.com']
 const user_Username = ['gasparboss', 'shawtyy']
-const user_Password = ['gasparboss123', 'shawtyyboss123']
+const user_Password = ['adminG123', 'adminS123']
+let loginUser = undefined
+let loginPassword = undefined
 
-
-passNo.addEventListener('click', function(){
+passNo.addEventListener('click', function () {
     passNo.style.display = 'none'
     passYes.style.display = 'block'
     passwordInput.type = 'text'
@@ -22,33 +24,24 @@ passYes.addEventListener('click', () => {
     passwordInput.type = 'password'
 })
 
-
-
-/* loginButton.addEventListener('click', () =>{
-    for (let i = 0; i < user_Username.length; i++){
-        if (emailInput.value === user_Username[i]){
-            error.style.display = 'flex'
-            error.style.backgroundColor = 'green'
-        } else{
-            error.style.display = 'flex'
-            error.style.backgroundColor = 'red'
-        }
-    }
-}) */
-
-loginButton.addEventListener('click', () =>{
+loginButton.addEventListener('click', () => {
+    
     let found = false;
     let i = 0
-    
-    if (emailInput.value.length !== 0){
-        while (i < user_Username.length){
-            if (emailInput.value === user_Username[i]){
+
+    if (emailInput.value.length !== 0 && passwordInput.value.length !== 0) {
+        while (i < user_Username.length) {
+            if ((emailInput.value === user_Username[i] || emailInput.value == user_Email[i]) && passwordInput.value === user_Password[i]) {
+                loginUser =  user_Username[i]
+                loginPassword = user_Password[i]
                 found = true
             }
             i += 1
         }
         if (found) {
-            null
+            error.style.display = 'none'
+            window.location.href = 'main.html'
+            
     
         } else {
             errorP.innerHTML = 'Helytelen felhasználónév vagy jelszó.'
@@ -57,7 +50,5 @@ loginButton.addEventListener('click', () =>{
     } else {
         error.style.display = 'none'
     }
-   
+
 });
-
-
